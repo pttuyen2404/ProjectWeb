71f203590e32b19142b9b5e5300c48a9e6db22f4
pttuyen2404 <106395953+pttuyen2404@users.noreply.github.com>
2023-06-23 06:54:42 +0700

    test

--- ownerServer.js
+++ ownerServer.js
@@ -21,3 +21,39 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Public thư mục ra ngoài
+app.use(express.static(__dirname + '/public'));
+
+// doi ten
+var hbs = exhbs.create({
+    defaultLayout: "owner",
+    extname: "hbs",
+    helpers: {
+        // so sanh chuoi
+        compareString(s1, s2, options) {
+            return s1 === s2 ? options.fn(this) : options.inverse(this);
+        },
+        // so sanh hai so
+        compareInt(int1, int2, options) {
+            return +int1 === +int2 ? options.fn(this) : options.inverse(this);
+        },
+        compareInt2(int1, int2, options) {
+            return +int1 != +int2 ? options.fn(this) : options.inverse(this);
+        },
+        forN(n, block) {
+            let acum = "";
+            for (let i = 1; i <= n; i++) {
+                acum += block.fn(i);
+            }
+            return acum;
+        },
+    }
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.listen(port, () => {
+    console.log(`Listen in port http://localhost:${port}`);
+});
--- server.js
+++ server.js
@@ -13,3 +13,50 @@ app.use(methodOverride('_method'));
 
 // import dotenv - bien moi truong
 require('dotenv').config();
+
+// import authenToken 
+const { authenToken, checkUserIsLogin, checkCurrentUser, authenTokenResApi } = require('./middlewares/authorizacation.Mw')
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Public thư mục ra ngoài
+app.use(express.static(__dirname + '/public'));
+
+
+// doi ten
+var hbs = exhbs.create({
+    defaultLayout: "main",
+    extname: "hbs",
+    helpers: {
+        // so sanh chuoi
+        compareString(s1, s2, options) {
+            return s1 === s2 ? options.fn(this) : options.inverse(this);
+        },
+        // so sanh hai so
+        compareInt(int1, int2, options) {
+            return +int1 === +int2 ? options.fn(this) : options.inverse(this);
+        },
+        compareInt2(int1, int2, options) {
+            return +int1 != +int2 ? options.fn(this) : options.inverse(this);
+        },
+        forN(n, block) {
+            let acum = "";
+            for (let i = 1; i <= n; i++) {
+                acum += block.fn(i);
+            }
+            return acum;
+        },
+    }
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+
+app.listen(port, () => {
+    console.log(`Listen in port http://localhost:${port}`);
+});
