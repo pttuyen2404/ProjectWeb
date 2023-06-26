96f450e5f2ed8f20b85b103a73e55666d65e4aaf
pttuyen2404 <106395953+pttuyen2404@users.noreply.github.com>
2023-06-26 23:37:20 +0700

    Update ownerServer.js

--- ownerServer.js
+++ ownerServer.js
@@ -54,6 +54,26 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
 
+
+// TẤT CẢ CÁC ROUTE
+
+// Route - Trang chủ
+app.use('/home', checkAuthAdmin, require("./controllers/Owner/Site/Site.C"));
+app.use('/', require("./controllers/Auth/logInOwner.C"));
+// Route - Đăng nhập
+app.use('/dangnhap', checkUserIsLoginAdmin, require("./controllers/Auth/logInOwner.C"));
+
+// Route - Đăng xuất
+app.use('/dangxuat', require("./controllers/Auth/logoutOwner.C"));
+//Route - Trang quản lí sản phẩm
+app.use('/allproduct', checkAuthAdmin, require("./controllers/Owner/product.C"));
+//Route - Trang quản lí đơn hàng
+app.use('/allorders', checkAuthAdmin, require("./controllers/Owner/order.C"));
+app.use('/statistics', checkAuthAdmin, require("./controllers/Owner/statistics.C"));
+
+//Route - Tất cả các trang còn lại
+app.use('*', require("./controllers/Site/whoop.C"));
+
 app.listen(port, () => {
     console.log(`Listen in port http://localhost:${port}`);
 });
